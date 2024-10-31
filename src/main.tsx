019ed9cb575c7c7cbf0d3c558489eeb3df4a00ec
Farothtc <mvkirimi@gmail.com>
2024-10-31 19:54:48 +0300

    React Router imported and installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: (
+      <div>
+        <h2 className="display-5 text-center mt-5">Error 404</h2>
+      </div>
+    ),
+  },
+  // {
+  //   path: "/:pokemonName",
+  //   element: <PokeStats />,
+  // },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
