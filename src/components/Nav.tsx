a7c6a356ee39f4f7e2d902af6d6197dc0b19d6d5
Farothtc <mvkirimi@gmail.com>
2024-11-04 04:50:08 +0300

    Search track, nav items, several icons added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export function Nav() {
+  const [searchItem, setSearchItem] = useState("");
+
+  console.log(searchItem);
+
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -14,11 +20,15 @@ export function Nav() {
           >
             <span className="navbar-toggler-icon" />
           </button>
-          <a className="navbar-brand" href="#">
-            Navbar
+          <a className="navbar-brand" href="/">
+            <img
+              src="./src/assets/netflix.svg"
+              alt="netflix logo"
+              id="netflix-logo-nav"
+            />
           </a>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto ms-4 mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
                   Home
@@ -26,12 +36,27 @@ export function Nav() {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link
+                  TV Shows
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
+                <a className="nav-link" href="#">
+                  Movies
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  New & Popular
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  My List
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Browse by Languages
                 </a>
               </li>
             </ul>
@@ -41,11 +66,30 @@ export function Nav() {
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
+                onChange={(event) => setSearchItem(event.target.value)}
               />
-              <button className="btn btn-outline-success" type="submit">
-                Search
+              <button
+                className="btn btn-outline-dark border-0"
+                type="submit"
+                id="icon-button"
+              >
+                <i className="bi bi-search"></i>
               </button>
             </form>
+            <div className="d-flex ms-2">
+              <button
+                className="btn btn-outline-dark border-0"
+                id="icon-button"
+              >
+                <i className="bi bi-bell"></i>
+              </button>
+              <button
+                className="btn btn-outline-dark border-0"
+                id="icon-button"
+              >
+                <i className="bi bi-person-circle"></i>
+              </button>
+            </div>
           </div>
         </div>
       </nav>
