b1e68593da92d5de91c4217eb5224b3dbb522a8e
Farothtc <mvkirimi@gmail.com>
2024-11-06 20:23:02 +0300

    Link added for route

--- src/components/ColMovies.tsx
+++ src/components/ColMovies.tsx
@@ -1,4 +1,5 @@
 import { MoviesData } from "../lib/Movies";
+import { Link } from "react-router-dom";
 
 type ColMoviesProps = {
   e: MoviesData;
@@ -12,18 +13,20 @@ export function ColMovies({ e }: ColMoviesProps) {
   return (
     <>
       <div className="col-md-3" id="col-movies">
-        <div className="card text-bg-dark">
-          <img src={e.banner} className="card-img" alt={e.name} />
-          <div className="card-img-overlay">
-            <h5 className="card-title" style={styles} id="col-movies-title">
-              {e.name}
-            </h5>
-            <p className="card-text">{e.featured && e.description}</p>
-            <p className="card-text">
-              <small>{e.featured && e.imdb + "/" + e.imdbmax}</small>
-            </p>
+        <Link to={`/${e.name.toLocaleLowerCase()}`}>
+          <div className="card text-bg-dark">
+            <img src={e.banner} className="card-img" alt={e.name} />
+            <div className="card-img-overlay">
+              <h5 className="card-title" style={styles} id="col-movies-title">
+                {e.name}
+              </h5>
+              <p className="card-text">{e.featured && e.description}</p>
+              <p className="card-text">
+                <small>{e.featured && e.imdb + "/" + e.imdbmax}</small>
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </>
   );
