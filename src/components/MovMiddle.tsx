0b17777d8df727fd105793d3c1efe0ec517474d3
Farothtc <mvkirimi@gmail.com>
2024-11-06 15:52:57 +0300

    Buttons added via bootstrap

--- src/components/MovMiddle.tsx
+++ src/components/MovMiddle.tsx
@@ -3,6 +3,16 @@ export function MovMiddle() {
     <>
       <div className="wrapper-middle">
         <h2>The Lord of the Rings The Return of The King</h2>
+        <div className="" id="button-wrapper-middle">
+          <button className="btn btn-light me-3" id="play-middle">
+            <i className="bi bi-play-fill"></i>
+            <span>Play</span>
+          </button>
+          <button className="btn btn-secondary" id="info-middle">
+            <i className="bi bi-info-circle"></i>
+            <span>More Info</span>
+          </button>
+        </div>
       </div>
     </>
   );
