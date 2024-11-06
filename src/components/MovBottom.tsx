47632a79cef371883c789e5dab7c36dc30ed7a66
Farothtc <mvkirimi@gmail.com>
2024-11-06 15:52:07 +0300

    Initial commit

---
+++ src/components/MovBottom.tsx
@@ -0,0 +1,22 @@
+export function MovBottom() {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <h2>Hello Bottom</h2>
+          </div>
+          <div className="col-md-3">
+            <h2>Hello Bottom</h2>
+          </div>
+          <div className="col-md-3">
+            <h2>Hello Bottom</h2>
+          </div>
+          <div className="col-md-3">
+            <h2>Hello Bottom</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
