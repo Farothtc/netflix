fa77cc460a0911b276a494edd271a3cc1e11379a
Farothtc <mvkirimi@gmail.com>
2024-11-06 15:51:59 +0300

    Movies bottom part component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { MovMiddle } from "./components/MovMiddle";
 import { Nav } from "./components/Nav";
+import { MovBottom } from "./components/MovBottom";
 
 function App() {
   return (
     <>
       <Nav />
       <MovMiddle />
+      <MovBottom />
     </>
   );
 }
